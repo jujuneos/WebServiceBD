24a1b9a229a3c1189e63a64de5d3ac5543fde318
jujuneos <lcjuniorconceicao@gmail.com>
2021-02-10 22:21:47 -0300

    configurando insert

--- src/main/java/com/deezefy/deezefy/Controllers/UsuarioController.java
+++ src/main/java/com/deezefy/deezefy/Controllers/UsuarioController.java
@@ -8,8 +8,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.deezefy.deezefy.Repositories.UsuarioRepository;
 import com.deezefy.deezefy.Services.UsuarioService;
 import com.deezefy.deezefy.dto.UsuarioDTO;
+import com.deezefy.deezefy.entities.Usuario;
 
 @RestController
 @RequestMapping(value = "/")
@@ -17,10 +19,19 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioService service;
+	@Autowired
+	private UsuarioRepository repo;
 	
 	@GetMapping(value = "users")
 	public ResponseEntity<List<UsuarioDTO>> findAll() {
 		List<UsuarioDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping(value = "insert")
+	public UsuarioDTO insert(UsuarioDTO dto){
+        Usuario usuario = new Usuario(dto.getEmail(), dto.getSenha(), dto.getData_nascimento());
+        usuario = repo.save(usuario);
+        return new UsuarioDTO(usuario);
+    }
 }
